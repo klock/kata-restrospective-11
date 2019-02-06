baea568eb6da2b06e9e674b7d80930c0689aad2c
Math Klock <math.klock@gmail.com>
2019-02-06 21:06:02 +0100

    question 1

--- src/main/java/fr/umlv/lexer/Lexer.java
+++ src/main/java/fr/umlv/lexer/Lexer.java
@@ -1,3 +1,19 @@
 package fr.umlv.lexer;
 
-// TODO
+import java.util.Objects;
+import java.util.Optional;
+
+class Lexer<T> {
+
+    private final static Lexer LEXER = new Lexer();
+
+    static Lexer create() {
+        return LEXER;
+    }
+
+    Optional<T> tryParse(String toBeParsed) {
+        Objects.requireNonNull(toBeParsed);
+
+        return Optional.empty();
+    }
+}
